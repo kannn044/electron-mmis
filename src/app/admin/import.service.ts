3e666e23b4e2a6d92c20a098d921faa12e864234
gun khemnak <gunnn_boy_hand@hotmail.com>
2018-04-26 11:01:19 +0700

    ผู้ประกอบการ

--- src/app/admin/import.service.ts
+++ src/app/admin/import.service.ts
@@ -6,8 +6,20 @@ export class ImportService {
 
   constructor() { }
 
-  createTmp(db: IConnection) {
+  createTmpPeople(db: IConnection) {
     const sql = `CREATE TABLE tmp_people (id int NOT NULL AUTO_INCREMENT,title_name varchar(255),fname varchar(255),lname varchar(255),position_name varchar(255),PRIMARY KEY(id))`;
+  
+    db.query(sql, function (error, results, fields) {
+      if (error) {
+        throw error;
+      }
+    });
+    return true;
+  }
+
+  createTmpLabeler(db: IConnection) {
+    const sql = `CREATE TABLE tmp_labelers (id int NOT NULL AUTO_INCREMENT,labeler_name varchar(255),description varchar(255),nin varchar(255),labeler_type varchar(255),labeler_status varchar(255),address varchar(255),tambon_code varchar(255),ampur_code varchar(255),province_code varchar(255),zipcode varchar(255),phone varchar(255),PRIMARY KEY (id))`;
+
     db.query(sql, function (error, results, fields) {
       if (error) {
         throw error;
@@ -29,25 +41,20 @@ export class ImportService {
   }
 
   importLabeler(db: IConnection, data: any) {
-    return new Promise((resolve, reject) => {
-      // db.query('CREATE TABLE tmp_labeler (id int NOT NULL AUTO_INCREMENT,labeler_name varchar(255),description varchar(255),nin varchar(255),labeler_type varchar(255),labeler_status varchar(255),address varchar(255),tambon_code varchar(255),ampur_code varchar(255),province_code varchar(255),zipcode varchar(255),phone varchar(255),PRIMARY KEY (id))');
-
-      // let sql = `INSERT INTO tmp_labeler (labeler_name,description,nin,labeler_type,labeler_status,address,tambon_code,ampur_code,province_code,zipcode,phone) VALUES ${data}`;
-
-      // db.query(sql, (error: any, results: any) => {
-      //   if (error) {
-      //     reject(error);
-      //   } else {
-      //     resolve(results);
-      //   };
-      // })
-      resolve(true);
-    })
+    data.forEach(v => {
+      const sql = `INSERT INTO tmp_labelers SET ?`;
+      db.query(sql, v, function (error, results, fields) {
+        if (error) {
+          throw error;
+        }
+      });
+    });
+    return true;
   }
 
   getTempLabeler(db: IConnection) {
     return new Promise((resolve, reject) => {
-      db.query(`SELECT * FROM tmp_labeler`, (error: any, results: any) => {
+      db.query(`SELECT * FROM tmp_labelers`, (error: any, results: any) => {
         resolve(results)
       })
     })
@@ -115,6 +122,18 @@ export class ImportService {
     return true;
   }
 
+  insertLabeler(db: IConnection, data: any) {
+    data.forEach(v => {
+      const sql = `INSERT INTO mm_labelers SET ?`;
+      db.query(sql, v, function (error, results, fields) {
+        if (error) {
+          throw error;
+        }
+      });
+    });
+    return true;
+  }
+
   deleteTempPeople(db: IConnection) {
     return new Promise((resolve, reject) => {
       db.query(`DROP TABLE tmp_people`, (error: any, results: any) => {
@@ -125,7 +144,17 @@ export class ImportService {
     })
   }
 
-  clearData(db: IConnection) {
+  deleteTempLabeler(db: IConnection) {
+    return new Promise((resolve, reject) => {
+      db.query(`DROP TABLE tmp_labelers`, (error: any, results: any) => {
+        if (error) {
+          reject(error);
+        } else resolve(results);
+      })
+    })
+  }
+
+  clearDataPeople(db: IConnection) {
     return new Promise((resolve, reject) => {
       db.query(`TRUNCATE TABLE um_people`, (error: any, results: any) => {
         if (error) {
@@ -134,10 +163,14 @@ export class ImportService {
       })
     })
   }
-  dbConnec(db: IConnection) {
-    db.connect();
-  }
-  dbClose(db: IConnection) {
-    db.end();
+
+  clearDataLabeler(db: IConnection) {
+    return new Promise((resolve, reject) => {
+      db.query(`TRUNCATE TABLE mm_labelers`, (error: any, results: any) => {
+        if (error) {
+          reject(error);
+        } else resolve(results);
+      })
+    })
   }
 }
